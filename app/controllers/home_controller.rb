ff35c8fae03861c6acde6b6d7384527745fc0472
sumit <iamsumitgoyal1@gmail.com>
2020-09-08 09:30:05 +0530

    Updated views and controller for products and orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,25 @@
 
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10, page_info: params[:page_info] })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 
   def productslisting
-    @productslist = ShopifyAPI::Product.find(:all)
-    @productslist.each do |product|
-      Product.create!(id: product.id, title: product.title, vendor: product.vendor)
-    end
+    @productslist = ShopifyAPI::Product.find(:all, params: { limit: 10, page_info: params[:page_info] })
+    # add_products(@productslist)
   end
 
   def orderslisting
-    @orderslist = ShopifyAPI::Order.find(:all)
+    @orderslist = ShopifyAPI::Order.find(:all, params: { limit: 2, page_info: params[:page_info] })
   end
+
+  # def add_products(products)
+  #   products.each do |product|
+  #     db_products = Product.new
+  #     db_products.title = product.title
+  #     db_products.title = product.title
+
+  #   end
+  # end
 end
